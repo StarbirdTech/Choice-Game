abfa2cad10be8a49ba90abffbb22caf3622a560d
nickthorn402 <98910010+nickthorn402@users.noreply.github.com>
2022-04-29 09:49:00 -0400

    Test.js

---
+++ Test.js
@@ -0,0 +1,16 @@
+let x = []
+let y = []
+
+function setup(){
+    for(let i; i < 3; i++){
+        for(let j; j < 3; j++){
+            x[i] = 0
+            y[i][j] = 0
+        }
+    }
+}
+
+function draw(){
+    background(100);
+    
+}
