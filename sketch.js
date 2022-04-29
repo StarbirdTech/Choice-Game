c92aefd1518bba2ce91c5111a2167dcca9a8cf8e
Starbird-Studios <64431622+StarbirdTech@users.noreply.github.com>
2022-04-29 08:54:38 -0400

    Added level switching

--- sketch.js
+++ sketch.js
@@ -1,59 +1,40 @@
-var boardRadius = 4;
-var size;
-var originHex;
-var hexes = [];
-var hex_color = [];
-var mainLayout;
-var count = 0;
+var currentLevel = 0;
 
-function setup()
-{
-  input = createInput();
-  input.position(20, 20);
-
-  button = createButton('Change Color');
-  button.position(input.x + input.width + 7, 20);
-  button.mousePressed(change_color);
-
-  createCanvas(windowWidth, windowHeight);
-  background(25);
-  angleMode(degrees);
-  size = Point(30,30);
-  originPixel = Point(width/2, height/2);
-  mainLayout = hexLayout(pointyOrient, size, originPixel)
-  hexGenerateBoard(boardRadius, hexes, Hex(0,0,0), hex_color);
-  originHex = Hex(0,0,0);
-  stroke(255);
+function setup () {
+    createCanvas(600, 600);
+    setupLevel();
 }
 
-function draw()
-{
-  noStroke()
-  background(50);
-  push();
-  translate(width/2, height/2);
-  hexDrawArray(mainLayout, hexes, hex_color);
-  pop();
-  fill(200);
-  text(hexes[0].q, 20, 60); // x
-  text(hexes[0].s, 40, 60); // y
-  text(hexes[0].r, 60, 60); // z
-  text(hexGetCoord(hexes[0]), 80, 60); // color
+function draw() {
+    drawLevel();
 }
 
-function change_color() {
-  hexNumTotal = hexes.length-1;
-  hexNum = input.value();
-  if (hexNum > hexNumTotal) {
-    hexNum = hexNumTotal;
-  }
-  else if (hexNum < 0) {
-    hexNum = 0;
-  }
-  hex_color[hexNum] = color(random(255), random(255), random(255));
-  count++
-  if (count > hexNumTotal)
-  {
-    count = 0;
-  }
+function setupLevel() {
+    switch(currentLevel) {
+        case 0:
+            background(0);
+            break;
+        case 1:
+            background(255, 0, 0);
+            break;
+    }
+}
+
+function drawLevel() {
+    switch(currentLevel) {
+        case 0:
+            circle(mouseX, mouseY, 10);
+            break;
+        case 1:
+            rect(mouseX, mouseY, 10);
+            break;
+    }
+}
+
+function mouseClicked() {
+    currentLevel++;
+    if (currentLevel > 1) {
+        currentLevel = 0;
+    }
+    setupLevel();
 }
