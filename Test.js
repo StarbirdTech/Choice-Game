49bca2ed6a385e071e59f8d97f8e0b0e1b0bc704
nickthorn402 <98910010+nickthorn402@users.noreply.github.com>
2022-04-29 14:21:28 -0400

    test

--- Test.js
+++ Test.js
@@ -1,16 +1,30 @@
-let x = []
-let y = []
+let x = [150, 300, 450]
+let y = [150, 300, 450]
+
+let squares = []
 
 function setup(){
-    for(let i; i < 3; i++){
-        for(let j; j < 3; j++){
-            x[i] = 0
-            y[i][j] = 0
-        }
-    }
+  createCanvas(600,600);
+  rectMode(CENTER)
+  squares.push(new square(150, 150, 120))
 }
 
 function draw(){
     background(100);
+    for(let i = 0; i < 3; i++){
+        for(let j = 0; j < 3; j++){
+            rect(x[i], y[j], 120)
+        }
+    }
+}
+
+class square{
+  constructor(i, j){
+    this.i = i
+    this.j = j
     
+    this.draw = function(){
+      rect(x[this.i], y[j], 120)
+    }
+  }
 }
