99431ed657a298ea3201ea646275952d17ad97eb
nickthorn402 <98910010+nickthorn402@users.noreply.github.com>
2022-05-03 13:02:44 -0400

    button

--- button.js
+++ button.js
@@ -9,7 +9,7 @@ function setup() {
     textSize(40);
     textAlign(CENTER)
 }
-
+  difference = 0;
   function draw(){
       background(100)
       fill(100)
@@ -18,21 +18,37 @@ function setup() {
           if(i>=49 | i<=4){stroke(0)} else {stroke(150)}
           ellipse(300, 400-i, 400, 100)
       }
-      fill(250, 0, 0)
-      noStroke()
+      fill(255, 0, 0)
       for (let j = 0; j < buttonHight; j++) {
-        if(j>=buttonHight-1 | j<=4){stroke(0)} else {stroke(250, 0, 0)}
+        if(j>=buttonHight-1 | j<=4){stroke(0)} else {stroke(230, 0, 0)}
         ellipse(300, 340-j, 300, 75)
     }
     text('Do Not Press This button', width/2, 100)
-    text(pressed, width/2, 200)
+    if(mouseDown){
+      if(difference <= 1){
+        buttonHight = lerp(50, 30, difference)
+        difference += 0.2
+        print('hehehehehe')
+      }
+    }
+    else{
+      if(difference >= 0){
+        buttonHight = lerp(50, 30, difference)
+        difference -= 0.2
+        print('hehehehehe')
+      }
+    }
   }
 
+  mouseDown = false;
+  
   function mousePressed(){
-    buttonHight -= 20
+    mouseDown = true;
+    //buttonHight -= 20
     pressed = true;
   }
 
   function mouseReleased(){
-    buttonHight += 20
+    mouseDown = false;
+    //buttonHight += 20
   }
