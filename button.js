4cc7e1844e4afd53f9fb6d1482bbc9f754855c02
nickthorn402 <98910010+nickthorn402@users.noreply.github.com>
2022-05-03 10:47:20 -0400

    button

--- button.js
+++ button.js
@@ -1,13 +1,38 @@
+let buttonHight = 50;
+let pressed = false;
+
 function setup() {
     createCanvas(600, 600);
     rectMode(CENTER);
     background(100);
-    textSize(60);
-    fill(0);
+    strokeWeight(5);
+    textSize(40);
     textAlign(CENTER)
-  
-  }
-  
+}
+
   function draw(){
-    
+      background(100)
+      fill(100)
+      stroke(150)
+      for (let i = 0; i < 50; i++) {
+          if(i>=49 | i<=4){stroke(0)} else {stroke(150)}
+          ellipse(300, 400-i, 400, 100)
+      }
+      fill(250, 0, 0)
+      noStroke()
+      for (let j = 0; j < buttonHight; j++) {
+        if(j>=buttonHight-1 | j<=4){stroke(0)} else {stroke(250, 0, 0)}
+        ellipse(300, 340-j, 300, 75)
+    }
+    text('Do Not Press This button', width/2, 100)
+    text(pressed, width/2, 200)
+  }
+
+  function mousePressed(){
+    buttonHight -= 20
+    pressed = true;
+  }
+
+  function mouseReleased(){
+    buttonHight += 20
   }
