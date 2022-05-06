612f456ed0dfce66d365dde3de220d9940ce5d10
Starbird-Studios <64431622+StarbirdTech@users.noreply.github.com>
2022-05-06 15:01:47 -0400

    lv 1 works

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function setup()
   sm.addScene ( level1 );
   sm.addScene ( level2 );
   sm.addScene ( level3 );
-  sm.addScene ( level4 );
+  //sm.addScene ( level4 );
   sm.addScene ( endScreen );
   sm.showNextScene();
 }
@@ -129,7 +129,7 @@ function level1() {
     gui = createGui();
     clicked = 0;
     stroke(0);
-    strokeWeight(10);
+    strokeWeight(5);
     fill(color('#EF2E72'));
     push()
     rectMode(CENTER)
@@ -138,12 +138,9 @@ function level1() {
     b = createButton("Skip", 450, 550);
     offsetX = rectSize*3/2;
     offsetY = rectSize*3/2;
-    //t = createCheckbox("Toggle", 100, 50, 200, 50);
-    // get index of checkbox
-    
     for (let squareY = 0; squareY <3; squareY++) {
       for (let squareX = 0; squareX <3; squareX++) {
-        checkBox = createCheckbox("grid", squareX * rectSize+offsetX, squareY * rectSize + offsetY, rectSize, rectSize);
+        checkBox.push(createCheckbox("grid", squareX * rectSize+offsetX, squareY * rectSize + offsetY, rectSize, rectSize));
       }
     }
     pop()
@@ -151,16 +148,35 @@ function level1() {
 
   this.draw = function() {
     background('#424549');
+    rectMode(CENTER);
+    fill(0);
+    rect(width/2, height/2, 200, 200);
     drawGui();
-    strokeWeight(10);
+    strokeWeight(5);
     textSize(48);
     textAlign(CENTER);
+    fill(color('#EF2E72'));
     text("Chose a square", width/2, 100)
+    for (let boxClicked = 0; boxClicked < checkBox.length; boxClicked++) {
+      if(checkBox[boxClicked].isChanged) {
+        for (let i = 0; i < checkBox.length; i++) {
+          if (i != boxClicked) {
+            checkBox[i].val = false;
+          }
+        }
+      }
+    }
   }
 
   this.mousePressed = function() {
     if (b.isPressed) {
-      lv1 = 'clicked';
+      for (let i = 0; i < checkBox.length; i++) {
+        if (checkBox[i].val == true) {
+          lv1 = i;
+          print(lv1);
+          break;
+        }
+      }
       sm.showNextScene();
     }
   }
