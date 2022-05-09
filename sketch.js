44486ab6f701da044e088955d1b423659e3c30c5
Starbird-Studios <64431622+StarbirdTech@users.noreply.github.com>
2022-05-09 09:43:29 -0400

    .

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup()
   sm.addScene ( level1 );
   sm.addScene ( level2 );
   sm.addScene ( level3 );
-  //sm.addScene ( level4 );
+  sm.addScene ( level4 );
   sm.addScene ( endScreen );
   sm.showNextScene();
 }
