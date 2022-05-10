69c8f57a68e3ae7f5177cdf6689cce807873c6f0
Starbird-Studios <64431622+StarbirdTech@users.noreply.github.com>
2022-05-10 10:19:30 -0400

    .

--- scenes/level4.js
+++ scenes/level4.js
@@ -7,6 +7,8 @@ function level4() {
   
   let gui;
 
+  let colorButton = [];
+
   this.enter = function() {
     background(100);
     gui = createGui();
@@ -14,25 +16,25 @@ function level4() {
     createCanvas(600, 600);
     background(100);
     strokeWeight(5);
-  }
-
-  this.draw = function() {
-    drawGui();
-
-    let index = -1;
+    let index = 0;
     for(let i = 0; i < 2; i++){
       for(let j = 0; j < 3; j++){
         index ++;
         x = (i * width) / 2 + 150;
         y = (j * (height-100)) / 3 + 125;
-        fill(r4[index], g4[index], b4[index]);
-        shape = new color();
-        shape.x = x;
-        shape.y = y;
-        rectMode(CENTER);
-        rect(shape.x, shape.y, 100, 100, 5);
+        colorButton.push(createButton('test', x, y, 100, 100));
+        
+        colorButton[].setStyle({
+          fillBg: "#FF0000"
+        });
+        
+        //colorButton.setStyle(fillbg, color(r4[index], g4[index], b4[index]));
       }
     }
+  }
+
+  this.draw = function() {
+    drawGui();
   }
 
   class color{
